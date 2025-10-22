c00806fdb39f76c8b12511661ac2ecb5c994e34e
Marcin Kaminski <marcin442228@gmail.com>
2025-10-22 22:52:13 +0100

    readdata

--- my-work/Topic02-representingdata/2.2-readdata.py
+++ my-work/Topic02-representingdata/2.2-readdata.py
@@ -1,10 +1,13 @@
+# This program reads in the "2.1-data.csv" file and outputs each line as a list.
+# Author: Marcin Kaminski
+
 import csv
 
 FILENAME = "2.1-data.csv"
 # DATADIR = "where did you put it"
 
+# with open (DATADIR + FILENAME, "rt") as fp:
 with open(FILENAME, "rt") as fp:
     reader = csv.reader(fp, delimiter =",")
     for line in reader:
         print(line)
-        
