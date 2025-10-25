603b01d48a834bbce628ed8a28e84bf59443d922
Marcin Kaminski <marcin442228@gmail.com>
2025-10-25 18:13:34 +0100

    readingfromurl

--- my-work/Topic03-acquiringdata/reading-from-url.py
+++ my-work/Topic03-acquiringdata/reading-from-url.py
@@ -6,4 +6,9 @@ import requests
 url = "https://www.gov.uk/bank-holidays.json"
 response = requests.get(url)
 data = response.json()
+print(data)
+# we can now analyze the data
+for event in data["northern-ireland"]["events"]:
+    print(f"{event}")
+    print(f"{event['title']} on {event['date']}")
 
