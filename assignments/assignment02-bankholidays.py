93dae7c850d9258d745a0ae3ffb3cd62746f273d
Marcin Kaminski <marcin442228@gmail.com>
2025-10-25 14:46:15 +0100

    assignment02

--- assignments/assignment02-bankholidays.py
+++ assignments/assignment02-bankholidays.py
@@ -1,9 +1,11 @@
-# This program prints out the dates of the bank holidays that happen in Northern Ireland.
+# This program fetches and prints out the dates of the bank holidays that happen in Northern Ireland.
 # Author: Marcin Kaminski
 
+import requests  # Import the requests module to handle HTTP requests
 import json # Import the JSON module to work with JSON data
 
-FILENAME = 'ukbankholidays.json' # Define the filename containing the JSON data
+#FILENAME = 'ukbankholidays.json' # Define the filename containing the JSON data
+URL = "https://www.gov.uk/bank-holidays.json" # Define the URL to fetch the JSON data from
 
 with open(FILENAME, 'rt') as file: # Open the JSON file for reading
     data = json.load(file)  # Load JSON data from the file and converts it into a Python dictionary
