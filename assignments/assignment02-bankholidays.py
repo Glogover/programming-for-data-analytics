328fbf8bb2ec22e64c4b62b4a0fb4aed28bbd957
Marcin Kaminski <marcin442228@gmail.com>
2025-10-25 14:41:19 +0100

    assignment02

--- assignments/assignment02-bankholidays.py
+++ assignments/assignment02-bankholidays.py
@@ -6,8 +6,8 @@ import json # Import the JSON module to work with JSON data
 FILENAME = 'ukbankholidays.json' # Define the filename containing the JSON data
 
 with open(FILENAME, 'rt') as file: # Open the JSON file for reading
-    data = json.load(file)  # Load JSON data from the file
+    data = json.load(file)  # Load JSON data from the file and converts it into a Python dictionary
     print("Bank holidays in Northern Ireland:") # Print a header message
     for holiday in data['northern-ireland']['events']: # Loop through each holiday
-        print(holiday['date']) # Print the date of the holiday
-
+        print(f"{holiday['date']}: {holiday['title']}") # Print the date and title of each holiday
+      
