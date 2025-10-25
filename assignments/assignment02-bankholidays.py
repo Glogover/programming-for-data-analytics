e2e87bd1e3e1dd8372653573cb29a8ad5fcd0670
Marcin Kaminski <marcin442228@gmail.com>
2025-10-25 14:20:35 +0100

    assignment02

--- assignments/assignment02-bankholidays.py
+++ assignments/assignment02-bankholidays.py
@@ -1,2 +1,11 @@
 # This program prints out the dates of the bank holidays that happen in Northern Ireland.
 # Author: Marcin Kaminski
+
+import json
+
+FILENAME = 'ukbankholidays.json'
+
+with open(FILENAME, 'rt') as file:
+    for line in file:
+        print(line, end="")
+        
