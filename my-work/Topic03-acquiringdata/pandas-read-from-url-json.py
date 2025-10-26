3b755dc39b9454276289fe193ee16d1989dffe45
Marcin Kaminski <marcin442228@gmail.com>
2025-10-26 14:43:35 +0000

    pandas read from json

--- my-work/Topic03-acquiringdata/pandas-read-from-url-json.py
+++ my-work/Topic03-acquiringdata/pandas-read-from-url-json.py
@@ -4,3 +4,11 @@
 import pandas as pd
 import json
 
+# this one does not work easily
+
+url = "https://api.open-meteo.com/v1/forecast?latitude=52.8436&longitude=-8.9864&daily=temperature_2m_max,temperature_2m_min"
+
+df = pd.read_json(url)
+
+print(df.head(3))
+
