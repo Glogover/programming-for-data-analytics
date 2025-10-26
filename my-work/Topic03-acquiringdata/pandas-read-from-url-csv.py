44cc5fd986c0178ffc6d8dc165f367abaacbf95c
Marcin Kaminski <marcin442228@gmail.com>
2025-10-26 14:01:46 +0000

    pandas read from url

--- my-work/Topic03-acquiringdata/pandas-read-from-url-csv.py
+++ my-work/Topic03-acquiringdata/pandas-read-from-url-csv.py
@@ -3,3 +3,11 @@
 
 import pandas as pd
 
+url = "https://api.open-meteo.com/v1/forecast?latitude=52.8436&longitude=-8.9864&daily=temperature_2m_max,temperature_2m_min"
+
+# or a different protocol (you will need to pip install S3Fs)
+url = "s3://noaa-gsod-pds/2020/72278023183.csv"
+
+df = pd.read_csv(url)
+print(df.head())
+
