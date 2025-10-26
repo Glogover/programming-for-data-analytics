d9e32004f170a8699abca906108ecc229cd81b92
Marcin Kaminski <marcin442228@gmail.com>
2025-10-26 20:59:45 +0000

    meteo setheader

--- my-work/Topic03-acquiringdata/pandas-meteo-setheader.py
+++ my-work/Topic03-acquiringdata/pandas-meteo-setheader.py
@@ -6,5 +6,7 @@ import pandas as pd
 url = "https://api.open-meteo.com/v1/forecast?latitude=52.8436&longitude=-8.9864&daily=temperature_2m_max,temperature_2m_min&format=csv"
 
 
-df = pd.read_csv(url)
-print(df.head)
+df = pd.read_csv(url, header = 2 )
+print(df.head(3))
+print(df.info())
+
