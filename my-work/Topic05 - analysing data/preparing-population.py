234f7f0ed412b8107e64b06d113c90e4a4329749
Marcin Kaminski <marcin442228@gmail.com>
2025-10-30 16:32:18 +0000

    prep population

---
+++ my-work/Topic05 - analysing data/preparing-population.py
@@ -0,0 +1,12 @@
+# Preparing the population data for analysis
+# The CSO data is quite clean 
+# All we really have to do is group it
+# Author: Marcin Kaminski
+
+import pandas as pd
+
+FILENAME = "cso-populationbyage.csv"
+#DATADIR = "../../data/"
+#FULLPATH = DATADIR + FILENAME
+
+
