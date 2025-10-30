59dc4289c57ab3a981efc8b7ec80c88433d7d36c
Marcin Kaminski <marcin442228@gmail.com>
2025-10-30 16:43:53 +0000

    prep population

--- my-work/Topic05 - analysing data/preparing-population.py
+++ my-work/Topic05 - analysing data/preparing-population.py
@@ -6,7 +6,14 @@
 import pandas as pd
 
 FILENAME = "cso-populationbyage.csv"
-#DATADIR = "../../data/"
+#DATADIR = "../../data/" # Two levels up from current directory and then into data folder
 #FULLPATH = DATADIR + FILENAME
+
+df = pd.read_csv(FILENAME)
+
+drop_col_list = ["Statistic Label","CensusYear","Sex","UNIT"]
+df.drop(columns=drop_col_list, inplace=True)
+
+print (df.head(3))
 
 
