b5a82fe5534ab5de7d1e76756142e99bcc7453d9
Marcin Kaminski <marcin442228@gmail.com>
2025-11-09 21:24:20 +0000

    standard deviation

--- my-work/Topic05 - analysing data/standard_deviation.py
+++ my-work/Topic05 - analysing data/standard_deviation.py
@@ -7,4 +7,7 @@ import pandas as pd
 temp1 = pd.Series([56, 65, 78, 86, 88, 92])
 temp2 = pd.Series9([33, 65, 78, 88, 92, 109])
 
-# 
+# print mean and standard deviation
+print(f'temp1: mean = {temp1.mean()}, standard deviation = {temp1.std()}')
+print(f'temp2: mean = {temp2.mean()}, standard deviation = {temp2.std()}'
+      )
