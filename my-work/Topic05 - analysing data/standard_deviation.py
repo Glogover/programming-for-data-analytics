d027a5f71f43226b63b4d83637ee7f6e463523c6
Marcin Kaminski <marcin442228@gmail.com>
2025-11-09 21:27:45 +0000

    standard deviation

--- my-work/Topic05 - analysing data/standard_deviation.py
+++ my-work/Topic05 - analysing data/standard_deviation.py
@@ -4,9 +4,9 @@
 import pandas as pd
 
 # create series
-temp1 = pd.Series([56, 65, 78, 86, 88, 92])
-temp2 = pd.Series([33, 65, 78, 88, 92, 109])
+temp1 = pd.Series([56, 65, 78, 86, 88, 92]) # temperatures in Fahrenheit
+temp2 = pd.Series([33, 65, 78, 88, 92, 109]) # temperatures in Fahrenheit
 
 # print mean and standard deviation
-print(f'temp1: mean = {temp1.mean()}, standard deviation = {temp1.std()}')
-print(f'temp2: mean = {temp2.mean()}, standard deviation = {temp2.std()}')
+print(f'temp1: mean = {temp1.mean()}, standard deviation = {temp1.std()}') # default is sample standard deviation
+print(f'temp2: mean = {temp2.mean()}, standard deviation = {temp2.std()}') # default is sample standard deviation
