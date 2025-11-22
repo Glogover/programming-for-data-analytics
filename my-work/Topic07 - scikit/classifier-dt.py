d9b4db0719a70803ac1d2cd941ca365e160b0f2c
Marcin Kaminski <marcin442228@gmail.com>
2025-11-22 23:11:50 +0000

    classifier

---
+++ my-work/Topic07 - scikit/classifier-dt.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+dataurl = "https://gist.githubusercontent.com/Thanatoz-1/9e7fdfb8189f0cdf5d73a494e4a6392a/raw/aaecbd14aeaa468cd749528f291aa8a30c2ea09e/iris_dataset.csv"
+
+irisdf = pd.read_csv(dataurl)
+irisdf.head(3)
+print(irisdf.head(3))
+
+
+
+
