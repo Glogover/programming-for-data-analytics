88feebc449e40c743c8a246254419d96fd0be675
Marcin Kaminski <marcin442228@gmail.com>
2025-11-22 23:33:13 +0000

    classifier

--- my-work/Topic07 - scikit/classifier-dt.py
+++ my-work/Topic07 - scikit/classifier-dt.py
@@ -3,7 +3,6 @@ import pandas as pd
 dataurl = "https://gist.githubusercontent.com/Thanatoz-1/9e7fdfb8189f0cdf5d73a494e4a6392a/raw/aaecbd14aeaa468cd749528f291aa8a30c2ea09e/iris_dataset.csv"
 
 irisdf = pd.read_csv(dataurl)
-irisdf.head(3)
 print(irisdf.head(3))
 
 colnames = ["sepal_length (cm)", "sepal_width (cm)", "petal_length (cm)", "petal_width (cm)"]
@@ -14,6 +13,7 @@ from sklearn import tree
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(x, y)
+
 
 
 
