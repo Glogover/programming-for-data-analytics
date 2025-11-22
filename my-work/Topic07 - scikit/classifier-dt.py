15e7c29cd78d1a4da6dcde1015f5510c37aef203
Marcin Kaminski <marcin442228@gmail.com>
2025-11-22 23:53:50 +0000

    classifier

--- my-work/Topic07 - scikit/classifier-dt.py
+++ my-work/Topic07 - scikit/classifier-dt.py
@@ -2,17 +2,25 @@ import pandas as pd
 
 dataurl = "https://gist.githubusercontent.com/Thanatoz-1/9e7fdfb8189f0cdf5d73a494e4a6392a/raw/aaecbd14aeaa468cd749528f291aa8a30c2ea09e/iris_dataset.csv"
 
-irisdf = pd.read_csv(dataurl)
-print(irisdf.head(3))
+irisdf = pd.read_csv(dataurl) # load dataset into a pandas dataframe
+print(irisdf.head(3)) # print first 3 rows of the dataframe
 
-colnames = ["sepal length (cm)", "sepal width (cm)", "petal length (cm)", "petal width (cm)"]
-x = irisdf[colnames] # input features, independent variables
+colnames = ["sepal length (cm)", "sepal width (cm)", "petal length (cm)", "petal width (cm)"] # feature column names
+x = irisdf[colnames] # input features, independent variables 
 y = irisdf["target"] # output label, target variable, dependent variable
 
-from sklearn import tree
+from sklearn import tree # import decision tree classifier
 
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(x, y)
+clf = tree.DecisionTreeClassifier() # create decision tree classifier object
+clf = clf.fit(x.values, y) # fit the model with input features and target variable
+
+
+print(clf.predict([[1,3,4,5]]))  # Example prediction
+
+print(clf.score(x.values, y))  # Model accuracy
+
+
+
 
 
 
