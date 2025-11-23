de0163666f8e5ea4cc4c21fc10db54d9cb464add
Marcin Kaminski <marcin442228@gmail.com>
2025-11-23 00:09:47 +0000

    classifier

--- my-work/Topic07 - scikit/classifier-dt.py
+++ my-work/Topic07 - scikit/classifier-dt.py
@@ -9,15 +9,18 @@ colnames = ["sepal length (cm)", "sepal width (cm)", "petal length (cm)", "petal
 x = irisdf[colnames] # input features, independent variables 
 y = irisdf["target"] # output label, target variable, dependent variable
 
-from sklearn import tree # import decision tree classifier
+# decision tree classifier
+#from sklearn import tree # import decision tree classifier
+#clf = tree.DecisionTreeClassifier() # create decision tree classifier object
 
-clf = tree.DecisionTreeClassifier() # create decision tree classifier object
+# kn classifier
+from sklearn.neighbors import KNeighborsClassifier # import k-nearest neighbors classifier
+clf = KNeighborsClassifier(n_neighbors=5) # create k-nearest neighbors classifier object with k=5
+
+
 clf = clf.fit(x.values, y) # fit the model with input features and target variable
-
-
-print(clf.predict([[1,3,4,5]]))  # Example prediction
-
-print(clf.score(x.values, y))  # Model accuracy
+print(clf.predict([[1,3,4,5]])) # Predict the class label for a new sample with features [1,3,4,5]
+print(clf.score(x.values, y))  # Print the accuracy of the model on the training data
 
 
 
