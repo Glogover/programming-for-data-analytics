cd75d6cb385e50c68f447fbc47d1ea72debafa4f
Marcin Kaminski <marcin442228@gmail.com>
2025-11-23 14:34:54 +0000

    regression

--- my-work/Topic07 - scikit/simpleresgression.py
+++ my-work/Topic07 - scikit/simpleresgression.py
@@ -3,10 +3,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # load the dataset
-data = sns.load_dataset('tips')
+dataset = sns.load_dataset('tips')
 
 # the first five entries of the dataset
-print(data.head())
+print(dataset.head())
 sns.set_style('whitegrid')
-sns.scatterplot(x='total_bill', y='tip', data=data)
+#sns.scatterplot(x='total_bill', y='tip', data=dataset)
+sns.lmplot(x='total_bill', y='tip', order=3, data=dataset)
 plt.show()
