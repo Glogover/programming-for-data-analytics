95d3b3e092f2724c910dda05cf9f6396347d0714
Marcin Kaminski <marcin442228@gmail.com>
2025-11-23 16:35:17 +0000

    lab 7.1

--- my-work/Topic07 - scikit/7.1-knock_weather.py
+++ my-work/Topic07 - scikit/7.1-knock_weather.py
@@ -1,6 +1,10 @@
 """In this lab I want to see if there is any relation between the windspeed and the month in Knock. 
 I am going to use the dataset
 "https://cli.fusio.net/cli/climate_data/webdata/mly4935.csv"""
+# Author: Marcin Kaminski
+
+# *** Import the libraries ***
+
 
 import pandas as pd
 import numpy as np
@@ -8,6 +12,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # *** Get the dataset ***
+
 
 # 1. Look at the csv file and realise that the first 19 lines do not contain data, so we need to skip them.
 
@@ -20,11 +25,35 @@ corrtemp = df["month"].corr(df["meant"])
 print("The correlation between the month and the mean temperature is:", corrtemp)
 
 # The answer is no, but that is for a number of reasons:
-# 1. The month is not a continuous variable, it is cyclical (after December comes January again)
-# 2. The temperature is also affected by the year, so we need to take that into account.
-# 3. There are missing values in the dataset, which can affect the correlation. 
+  # 1. The month is not a continuous variable, it is cyclical (after December comes January again)
+  # 2. The temperature is also affected by the year, so we need to take that into account.
+  # 3. There are missing values in the dataset, which can affect the correlation. 
 
 # *** Tidy the wind data ***
+
+
+# 3. If you look at the data you will see that there are some windspeeds missing, we need to drop those rows, lets make a new dataset which only has the month and wind speed.
+
+# https://stackoverflow.com/questions/29314033/drop-rows-containing-empty-cells-from-a-pandas-dataframe
+
+# 4. Great, now lets drop the NAs
+cleandf = df[["month", "wdsp"]] # make a new dataframe with only the month and windspeed
+# replace the spaces with NaN
+cleandf['wdsp']= cleandf.loc[:,('wdsp')].replace(' ', np.nan) 
+cleandf.dropna(inplace=True) # drop the rows with NaN values
+
+# print(cleandf.head(3)) # check the new dataframe
+
+# Now we just need to convert the wind speed to floats
+cleandf["wdsp"] = cleandf["wdsp"].astype(float) # convert the wind speed to floats
+
+
+
+
+
+
+
+
 
 
 
